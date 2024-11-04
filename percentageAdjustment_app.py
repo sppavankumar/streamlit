c876bd83250f4edc2c1171250b02b4a12f17bbb8
Pavan Kumar <ps@Pavans-MacBook-Pro.local>
2024-11-03 22:43:50 -0600

    v0.1 initial version

---
+++ percentageAdjustment_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+def pctAmount(amount:float, pct:float) -> float:
+    percentageAmount = (pct/100)*amount
+    return amount-percentageAmount
+
+def main():
+    st.title("Determine amount after percentage adjustment")
+    
+    amount_input = st.number_input("Enter the total amount",min_value=1)
+    pct_input = st.number_input("Enter the percentage to be applied",min_value=1)
+
+    if st.button("Calculate"):
+        if amount_input > 0 and pct_input > 0:
+            finalAmount = pctAmount(amount_input,pct_input)
+            st.success(f"\${amount_input:.2f} adjusted to {pct_input:.2f}\% = **\${finalAmount:.2f}**") 
+        else:
+            st.warning("Please enter number greater than zero")
+    else:
+        st.error("Please click on the button")
+    
+if __name__ == "__main__":
+    main()
